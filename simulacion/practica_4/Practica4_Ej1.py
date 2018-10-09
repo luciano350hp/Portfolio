12e01bb5970a332c8ffaaaf8890d6402a84bb054
luciano350hp <luciano350hp@gmail.com>
2018-10-09 20:43:36 -0300

    poco avance

--- simulacion/practica_4/Practica4_Ej1.py
+++ simulacion/practica_4/Practica4_Ej1.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python
 
-import numpy as np
+import numpy as np 
+
+#Ejercicio 1
+def discretizar(L, T, delta):
+    """ La funcion devuelve los array con el dominio
+        de x y t discretizados. """
+    t = (np.array(np.arange(0, T + delta, delta, dtype = float)))
+    x = (np.array(np.arange(0, L + delta, delta, dtype = float)))
+    return(t, x)
+    
+b = discretizar(20, 5, 0.5)
+print("El vector discreto es: \n ", b, "\n")
