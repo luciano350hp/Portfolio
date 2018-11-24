6397fb94f1db18735a0b217cf815d1db0b116809
luciano350hp <luciano350hp@gmail.com>
2018-11-24 20:01:43 -0300

    falta punto 3 y 4

--- simulacion/practica_6/Practica6_Ej2_5.py
+++ simulacion/practica_6/Practica6_Ej2_5.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import animation
+from random import shuffle
 
-def bernouli(p):
-	num =  np.random.rand()
-	print(num)
-	if (num < p):
-		return 1
-	else:
-		return 0
-print(bernouli(1/2)) 
+def hipergeometrica(r, b, k):
+	listaBolitas = []
+	for j in range (0, r): #Pongo las bolitas rojas en la bolsa
+		listaBolitas.append('red')
+	for i in range (0, b): #Pongo las bolitas azules en la bolsa
+		listaBolitas.append('blue')
+	shuffle(listaBolitas) #Las mezclo de forma random
+	print("Las bolitas en la bolsa son: ",np.array(listaBolitas))
+	listaBolitask = np.random.choice(listaBolitas, k, replace=False)
+	print("Se tomaron",k,"bolitas: ", listaBolitask)
+	listaRojas = list(filter(lambda x: (x == 'red') , listaBolitask))
+	print("De esa muestra hay",len(listaRojas),"bolitas rojas")
+hipergeometrica(10, 7, 5) 
 		
 	
