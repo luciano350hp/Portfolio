802674e2514cea9044924a0ba78f0fffe0b70334
luciano350hp <luciano350hp@gmail.com>
2018-11-26 15:49:26 -0300

    tp graciana prueba

--- TPFINAL/src/peliculas/PELICULA.java
+++ TPFINAL/src/peliculas/PELICULA.java
@@ -3,19 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication2;
+package peliculas;
 
 /**
  *
  * @author Luciano
  */
-public class JavaApplication2 {
+public class PELICULA {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
