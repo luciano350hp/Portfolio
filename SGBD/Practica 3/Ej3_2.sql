ef9209c7ae044e7c3f869335d9e6d23187664b0e
luciano350hp <luciano350hp@gmail.com>
2019-05-05 00:43:09 -0300

    hola

--- SGBD/Practica 3/Ej3_2.sql
+++ SGBD/Practica 3/Ej3_2.sql
@@ -20,7 +20,26 @@ SELECT governmentform, COUNT (*) AS cantidad FROM country GROUP BY governmentfor
 SELECT continent, SUM(surfacearea) AS superficie FROM country GROUP BY continent ORDER BY superficie DESC;
 
 \echo CONSULTA 3.2.7 
-SELECT continent, COUNT(*) AS cantidad FROM country WHERE (population > 2000000) GROUP BY continent HAVING (COUNT (*) > 15);
+SELECT continent, COUNT(*) AS cantidad FROM country WHERE (population > 20000000) GROUP BY continent HAVING (COUNT (*) > 15);
 
-\echo Subqueries
-SELECT name , lifeexpectancy FROM country WHERE lifeexpectancy = (SELECT MIN (lifeexpectancy) FROM country )
+\echo Subqueries 1
+SELECT name , lifeexpectancy FROM country WHERE lifeexpectancy = (SELECT MIN (lifeexpectancy) FROM country);
+
+\echo Subqueries 2
+SELECT name, lifeexpectancy FROM country WHERE lifeexpectancy = (select min(lifeexpectancy) from country) OR lifeexpectancy = (select max(lifeexpectancy) from country);  
+
+\echo Subqueries 3
+SELECT name, indepyear FROM country WHERE continent = (select continent from country where indepyear = (select min(indepyear) from country));
+
+\echo Subqueries 4
+SELECT DISTINCT continent FROM country WHERE continent IN (select continent FROM country ORDER BY gnp DESC LIMIT 10);
+
+\echo Join 1
+SELECT name, language FROM country, countrylanguage WHERE code = countrycode AND continent = 'Oceania';
+
+\echo Join 2
+SELECT name, COUNT(language) AS lenguas FROM country INNER JOIN countrylanguage ON (code = countrycode) GROUP BY name HAVING COUNT(language) > 1 ORDER BY lenguas DESC;
+
+\echo Join 3
+SELECT DISTINCT language from country,countrylanguage where code = countrycode AND continent = (SELECT continent FROM country GROUP BY continent HAVING SUM(gnp) > 0 ORDER BY SUM(gnp) LIMIT 1);
+
