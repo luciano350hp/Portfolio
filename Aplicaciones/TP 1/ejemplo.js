06efb09b3e133c94be31822f0455d714d30d6796
luciano350hp <luciano350hp@gmail.com>
2019-10-04 23:01:36 -0300

    Falta Factorial

--- Aplicaciones/TP 1/ejemplo.js
+++ Aplicaciones/TP 1/ejemplo.js
@@ -17,8 +17,42 @@ class Calculadora {
 		div = [this.a/this.b, this.a%this.b]
 		return div
 	}
+	multiplicacion(){
+		let mult = 0;
+		return this.a * this.b
+	}
+	resta(){
+		let resta = 0; 
+		return this.a - this.b;
+	}
+	suma(){
+		let suma = 0;
+		return this.a + this.b;
+	}
+	*ejemplo(input) {
+		var doubleThat = 2 * (yield (input / 2))
+		var another = yield (doubleThat)
+		return (input * doubleThat * another)
+	}
+	*factorial (number, acc = 1) {
+		yield acc
+		if (number > 1) 
+		yield *factorial(number - 1, acc * number);
+		}
+
 }
+
+
 
 c = new Calculadora (20,15)
 const [resultado, resto] = c.division()
-console.log(resultado,resto)
+console.log("La multiplicación es: ",c.multiplicacion());
+console.log("La suma es: ",c.suma());
+console.log("La resta es: ",c.resta());
+console.log("El resultado de la división es: ",resultado, "Con resto: ",resto)
+
+
+
+
+
+
